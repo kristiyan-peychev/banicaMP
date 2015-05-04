d58d96a641a277570fb124238a7c94ab4481aa0a
Kristiyan Peychev <kurostuki_l@yahoo.com>
2015-05-05 00:36:10 +0300

    Formatting

---
+++ decode/get.h
@@ -0,0 +1,18 @@
+#ifndef GET_R1S6CD6W
+
+#define GET_R1S6CD6W
+
+#include "decoder.h"
+
+// FIXME remove hardcode
+decoder *get_decoder(FILE *file, const char *enc_type)
+{
+	if (!strncmp(enc_type, "FLAC", sizeof("FLAC")))
+		return new flac_decoder(file);
+	else if (!strncmp(enc_type, "MP3", sizeof("MP3")))
+		return new MPEG_decoder(file);
+	else
+		return NULL;
+}
+
+#endif /* end of include guard: GET_R1S6CD6W */
