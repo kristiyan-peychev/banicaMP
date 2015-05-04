9756d5dc84bd9ef1ace0e6865669f204adeea492
Kristiyan Peychev <kurostuki_l@yahoo.com>
2015-05-04 03:19:11 +0300

    Tests for play.cpp

--- test/test_play.cpp
+++ test/test_play.cpp
@@ -1,5 +1,22 @@
 #include "../playback/play/play.h"
 
+#define WAV_FPATH "/home/kawaguchi/test.wav"
+
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+
 int main(int argc, const char *argv[]) {
+	FILE *fp = fopen(WAV_FPATH, "r");
+	play_wav *player = get_player(fp);
+	player->begin();
+	sleep(10);
+	player->pause();
+	sleep(5);
+	player->pause();
+	player->play();
+	sleep(5);
+	player->toggle_pause();
+	player->toggle_pause();
 return 0;
 }
