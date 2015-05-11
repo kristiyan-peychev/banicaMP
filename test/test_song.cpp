ecc89a9f4d683f2f731dbf92c70d5ff5307da9f8
HuKCaHa <nikolaykgeorgiev@gmail.com>
2015-05-11 22:56:22 +0300

    changed test_song

--- test/test_song.cpp
+++ test/test_song.cpp
@@ -5,8 +5,6 @@
 #define MP3_SONG_PATH "/home/nikolay/ACDC - Whole Lotta Rosie.mp3"
 #define FLAC_SONG_PATH "/home/nikolay/31. Jinsei wa Belt Conveyor no Youni Nagareru.flac"
 
-#include <iostream>
-using namespace std;
 
 int main(int argc, const char *argv[]) {
 	song mp3(MP3_SONG_PATH);
@@ -14,9 +12,7 @@ int main(int argc, const char *argv[]) {
     printf("%s\n", flac.get_path());
     printf("%s\n", mp3.get_encoding());
     printf("%s\n", flac.get_encoding());
-    cout<<flac.get_info().title<<endl;
     flac.start();
-   // printf("%s\n", mp3.get_info()->tag->title().toCString(true));
 
     return 0;    
 }
