4af9160aa09266d1831ddb588295f9b89efea58e
HuKCaHa <nikolaykgeorgiev@gmail.com>
2015-05-15 00:41:43 +0300

    song are now playing (sort of...)

--- playback/playlist/song.cpp
+++ playback/playlist/song.cpp
@@ -1,48 +1,63 @@
 #include "song.h"
 #include "encodings.h"
 
-song::song(const char* path): info(path), decoded_file(NULL), player(NULL)
+song::song(const char* p): info(p),path(NULL), dec(NULL), decoded_file(NULL),f(NULL), player(NULL)
 {
 	
-	this->path = new char[strlen(path)+1];
-	std::strcpy(this->path, path);
-    f = fopen(this->path, "r");
-	this->encoding = get_file_encoding(path);	
-    this->dec = get_decoder(f,this->encoding); 
+	path = new char[strlen(p)+1];
+	std::strcpy(path, p);
+	encoding = get_file_encoding(path);
+
     
 }
 
 song::~song()
 {
-	delete[] path;
-    fclose(f);
+    if(path != NULL)
+	    delete[] path;
     clear_song();
-    if(player != NULL)
-        delete player;
-    
-    if(dec != NULL)
-        delete dec;
-
 }
 
 void song::decode_song()
 {
-    if(decoded_file == NULL){
-        decoded_file = tmpfile();
-        dec->decode(decoded_file);
+    if(dec == NULL){
+        f = fopen(path, "rb");
+        dec = get_decoder(f, encoding);
+    }
+
+    if(player == NULL){
+        if(decoded_file == NULL){
+            tmpnam(tmp_file_name);
+            decoded_file = fopen(tmp_file_name, "w+b");;
+            dec->decode(decoded_file);
+            fclose(decoded_file);
+        }
+
+        decoded_file = fopen(tmp_file_name, "r+b");
         player = get_player(decoded_file);
-        
     }
 }
 
 void song::clear_song()
 {
-    if(decoded_file != NULL)
+    if(decoded_file != NULL){
         fclose(decoded_file);
+        remove(tmp_file_name);
+    }
+    if(dec != NULL)
+        delete dec;
+    //CAUSES DOUBLE FREE OR CORRUPTION
+    //WTF
+   // if(f != NULL)
+     //   fclose(f);
+
     if(player != NULL)
         delete player;
+    
     player = NULL;
     decoded_file = NULL;
+    f = NULL;
+    dec = NULL;
     
 }
 
@@ -67,15 +82,19 @@ void song::start()
 {
     if(player == NULL)
         decode_song();
+
     try{
-    player->begin();
-    #ifdef PLAY_J01D18YC
+        player->begin();
+        printf("4\n");
+        #ifdef PLAY_J01D18YC
+        printf("5\n");
         wait(NULL);
-    #endif
+        #endif
+        printf("ffdf\n");
     } catch ( playbackend_except &e){
         //NO IDEA WHAT TO DO HERE YET
+        printf("ffffff\n");
     }
-    
 
 }
 
@@ -91,5 +110,7 @@ void song::stop()
     if(player == NULL)
         throw player_not_found_exception();
     player->stop();
+    //TODO
+    clear_song();
 }
 
