924db79943c0d4d03a610f3afd3689b68a5a3524
HuKCaHa <nikolaykgeorgiev@gmail.com>
2015-05-19 20:07:40 +0300

    using std::thread to play song

--- playback/playlist/song.cpp
+++ playback/playlist/song.cpp
@@ -1,13 +1,14 @@
 #include "song.h"
 #include "encodings.h"
+#include "sys/wait.h"
 
-song::song(const char* p): info(p),path(NULL), dec(NULL), decoded_file(NULL),f(NULL), player(NULL)
+song::song(const char* p): info(p),path(NULL), dec(NULL), decoded_file(NULL),
+song_file(NULL), player(NULL)
 {
 	
 	path = new char[strlen(p)+1];
 	std::strcpy(path, p);
 	encoding = get_file_encoding(path);
-
     
 }
 
@@ -18,16 +19,17 @@ song::~song()
     clear_song();
 }
 
-void song::decode_song()
+void song::load_song()
 {
     if(dec == NULL){
-        f = fopen(path, "rb");
-        dec = get_decoder(f, encoding);
+        song_file = fopen(path, "r+b");
+        dec = get_decoder(song_file, encoding);
     }
 
     if(player == NULL){
         if(decoded_file == NULL){
             tmpnam(tmp_file_name);
+            printf("%s\n", tmp_file_name);
             decoded_file = fopen(tmp_file_name, "w+b");;
             dec->decode(decoded_file);
             fclose(decoded_file);
@@ -35,7 +37,7 @@ void song::decode_song()
 
         decoded_file = fopen(tmp_file_name, "r+b");
         player = get_player(decoded_file);
-    }
+            }
 }
 
 void song::clear_song()
@@ -50,13 +52,15 @@ void song::clear_song()
     //WTF
    // if(f != NULL)
      //   fclose(f);
-
+    
+    //if(song_file != NULL)
+      //  fclose(song_file);
     if(player != NULL)
         delete player;
     
     player = NULL;
     decoded_file = NULL;
-    f = NULL;
+    song_file = NULL;
     dec = NULL;
     
 }
@@ -78,39 +82,33 @@ const char* song::get_encoding() const
 	return this->encoding;
 }
 
+void start_thread(song& s){
+    s.player->begin();
+    s.clear_song();
+    //throw end_of_song_exception();
+}
+
 void song::start()
 {
     if(player == NULL)
-        decode_song();
+        load_song();
 
-    try{
-        player->begin();
-        printf("4\n");
-        #ifdef PLAY_J01D18YC
-        printf("5\n");
-        wait(NULL);
-        #endif
-        printf("ffdf\n");
-    } catch ( playbackend_except &e){
-        //NO IDEA WHAT TO DO HERE YET
-        printf("ffffff\n");
-    }
-
+    t = new std::thread(&start_thread, std::ref(*this));
+    t->detach();
+    printf("%d fff\n",t->joinable());
 }
 
 void song::pause()
 {
-    if(player == NULL)
-        throw player_not_found_exception();
-    player->toggle_pause();
+    //if(player == NULL)
+        //throw player_not_found_exception();
+        player->toggle_pause();
 }
 
 void song::stop()
 {
-    if(player == NULL)
-        throw player_not_found_exception();
-    player->stop();
-    //TODO
-    clear_song();
+    //if(player == NULL)
+        //throw player_not_found_exception();
+        player->stop();
 }
 
--- playback/playlist/song.h
+++ playback/playlist/song.h
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <cstdio>
 #include <exception>
+#include <thread>
 
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
@@ -26,6 +27,13 @@ public:
     virtual const char* what() const throw()
     {
         return "Cannot read tags";
+    }
+};
+class end_of_song_exception : public std::exception {
+public:
+    virtual const char* what() const throw()
+    {
+        return "End of song";
     }
 };
 
@@ -73,10 +81,11 @@ private:
 	const char* encoding;
 	decoder* dec;
     FILE* decoded_file;
-    FILE* f;
+    FILE* song_file;
     play_wav* player;
+    std::thread* t;
 
-    void decode_song();
+    void load_song();
     void clear_song();
 
 public:
@@ -92,6 +101,8 @@ public:
     void start();
     void pause();
     void stop();
+
+    friend void start_thread(song&);
 };
 
 #endif
