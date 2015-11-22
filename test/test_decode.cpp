64de817f14dd1de291943f72fe74a0d525664b7a
Kristiyan Peychev <kurotsuki_l@yahoo.com>
2015-11-22 15:42:45 +0200

    Modifying the decoder tests to match the new memory model; decoder tests now accept the song to decode as an argument

--- test/test_decode.cpp
+++ test/test_decode.cpp
@@ -1,14 +1,14 @@
 #include "../decode/decoder.h"
 #include "../decode/get.h"
 #include <cstdio>
-
-#define file2 "/home/nikolay/31. Jinsei wa Belt Conveyor no Youni Nagareru.flac"
-#define file "/home/nikolay/ACDC - Whole Lotta Rosie.mp3"
-#define Ofile "/home/kawaguchi/amfg.wav"
-
-#define fi "/falos/11 The Ulimate.mp3"
+#include <cstdlib>
 
 int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: `%s <song path>`\n", *argv);
+        exit(1);
+    }
+	FILE *f = fopen(argv[1], "r");
 	FILE *f = fopen(fi, "r");
 	decoder *what = get_decoder(f, ENC_MP3);
 	FILE *ofile = fopen(Ofile, "w+");
--- test/test_decode3.cpp
+++ test/test_decode3.cpp
@@ -3,14 +3,15 @@
 
 #include "../memory/memory.h"
 #include <cstdio>
-
-#define fi "/falos/11 The Ulimate.mp3"
+#include <cstdlib>
 
 int main(int argc, const char *argv[]) {
-	FILE *f = fopen(fi, "r");
-    memory mem(640000000);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: `%s <song path>`\n", *argv);
+    }
+	FILE *f = fopen(argv[1], "r");
+    memory_ref mem(640000000);
 	decoder *what = get_decoder(f, ENC_MP3);
-	//what->decode(ofile);
     what->decode(&mem);
 return 0;
 }
