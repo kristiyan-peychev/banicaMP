93d98e208289b2f0968be1b2d3b49ecc76662e64
Kristiyan Peychev <kurotsuki_l@yahoo.com>
2016-04-10 13:53:45 +0300

    Modified test_playtlist to now get the directory it scans via its first argument, added proper indentation, and stylized if-elseif-else braces

--- test/test_playlist.cpp
+++ test/test_playlist.cpp
@@ -15,36 +15,41 @@ void print_help(){
     printf("quit - Exit the program\n");
 }
 
-int main(){
-    playlist p("/media/win/Downloads/Music/OST.Avengers.Assemble.VA.2012.MP3.320kbps - SMG/");
+int main(int argc, char *argv[]){
+    if (argc != 2) {
+        fprintf(stderr, "Usage: `%s <directory to open>`\n");
+        exit(1);
+    }
+    playlist p(argv[1]);
     observer.set_playlist(&p);
     p.print_songs();
     char command[20];
     int a;
     while(true){
-        try{
         printf("\n> ");
         scanf("%s",command);
-        if(!strcmp(command,"list"))
-            p.print_songs();
-        else if(!strcmp(command, "play")){
-            scanf("%d", &a);
-            p.play_song(a);
-        }else if(!strcmp(command, "pause"))
-            p.pause_song();
-        else if(!strcmp(command, "seek")){
-            scanf("%d", &a); 
-            p.seek(a);
-        }else if(!strcmp(command, "stop"))
-            p.stop_song();
-        else if(!strcmp(command, "shuffle"))
-            p.toggle_shuffle();
-        else if(!strcmp(command, "repeat"))
-            p.toggle_repeat();
-        else if(!strcmp(command, "help"))
-            print_help();
-        else if(!strcmp(command, "quit"))
-            break;
+        try{
+            if(!strcmp(command,"list")) {
+                p.print_songs();
+            } else if(!strcmp(command, "play")) {
+                scanf("%d", &a);
+                p.play_song(a);
+            } else if(!strcmp(command, "pause")) {
+                p.pause_song();
+            } else if(!strcmp(command, "seek")) {
+                scanf("%d", &a);
+                p.seek(a);
+            } else if(!strcmp(command, "stop")) {
+                p.stop_song();
+            } else if(!strcmp(command, "shuffle")) {
+                p.toggle_shuffle();
+            } else if(!strcmp(command, "repeat")) {
+                p.toggle_repeat();
+            } else if(!strcmp(command, "help")) {
+                print_help();
+            } else if(!strcmp(command, "quit")) {
+                break;
+            }
         } catch(...){
             printf("Exception\n");
             p.stop_song();
