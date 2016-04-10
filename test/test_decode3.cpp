14a927c1b23f91edbf3aa17a282f21be43c4a62d
Kristiyan Peychev <kurotsuki_l@yahoo.com>
2016-04-10 13:54:42 +0300

    Added a possible third argument to test_decode3

--- test/test_decode3.cpp
+++ test/test_decode3.cpp
@@ -6,8 +6,8 @@
 #include <cstdlib>
 
 int main(int argc, const char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: `%s <song path>`\n", *argv);
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "Usage: `%s <song path> [<filedump>]`\n", *argv);
         exit(1);
     }
 	FILE *f = fopen(argv[1], "r");
@@ -16,5 +16,9 @@ int main(int argc, const char *argv[]) {
 	decoder *what = get_decoder(f, ENC_MP3);
     what->decode(mem);
     fprintf(stderr, "Final memory size is %d bytes\n", mem.cap());
+    if (argc == 3) {
+        FILE *file = fopen(argv[2], "wb+");
+        fwrite(mem.begin(), 1, mem.cap(), file);
+    }
 return 0;
 }
